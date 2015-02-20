b583df515d7c309217924c5a3a3266f95a8fb49b
peterswift <P.M.Swift@warwick.ac.uk>
2015-02-20 11:38:24 +0000

    Update TradesThread.java
    Added code to append, include date in file name, create file if not detected etc.

--- src/com/cs261/main/TradesThread.java
+++ src/com/cs261/main/TradesThread.java
@@ -1,72 +1,92 @@
-package com.cs261.main;
+package mainCode;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.ParseException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.lang.Runnable;
+import sun.misc.SignalHandler;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 // This code takes the input stream and then outputs it to a csv file. However now does it within a thread so other code may be used. Also it will now run until an interrupt is used (Ctrl-C).
+
 
 
 public class TradesThread implements Runnable {
     public void run() { // Threads running code.
-        try {
-            System.out.println("In the trades thread");
+	try {
+	  System.out.println("In the trades thread");
 
-            BufferedReader inputTrades = null;
-            //Define a buffer for listening to the socket.
-            final PrintWriter outputTrades = new PrintWriter(new FileWriter("tradesstore.csv"));
-            //Define a writter for outputting trades.
+	  DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+	   //get current date time with Date()
+	  Date currentdate = new Date();
+	  String dateStr = dateFormat.format(currentdate);
+	  boolean DoesExist = new File("tradesstore" + dateStr + ".csv").exists();
+	  final PrintWriter outputTrades = new PrintWriter(new FileWriter("tradesstore" + dateStr + ".csv",true));
+	  //Define a writer for outputting trades.
 
-            String hostName = "cs261.dcs.warwick.ac.uk";
-            // Declare the host for the socket.
-            int portNumber = Integer.parseInt("80");
-            // Declare the port number to listen to.
-            final Socket echoSocket = new Socket(hostName, portNumber);
-            // Establish a socket using the chosen host and port.
+	  String hostName = "cs261.dcs.warwick.ac.uk";
+	  // Declare the host for the socket.
+	  int portNumber = Integer.parseInt("80");
+	  // Declare the port number to listen to.
+	  final Socket echoSocket = new Socket(hostName, portNumber);
+	  // Establish a socket using the chosen host and port.
 
-            Runtime.getRuntime().addShutdownHook(new Thread() {
-                // Code run when program is stopped via Ctrl+c
-                public void run() {
-                    try {
-                        System.out.println("Ctrl-c caught");
-                        if (outputTrades != null) {
-                            outputTrades.close(); // Close link to file output.
-                        }
-                        if (echoSocket != null) {
-                            echoSocket.close(); // Close link to socket.
-                        }
-                    } catch (IOException IOEx) {
-                        IOEx.printStackTrace();
-                    }
-                }
+	  Runtime.getRuntime().addShutdownHook(new Thread() {
+	      // Code run when program is stopped via Ctrl+c
+	      public void run() {
+		try {
+		  System.out.println("Ctrl-c caught");
+		  if (outputTrades != null) {
+		      outputTrades.close(); // Close link to file output.
+		  }
+		  if (echoSocket != null) {
+		      echoSocket.close(); // Close link to socket.
+		  }
+		} catch (IOException IOEx){
+		  IOEx.printStackTrace();
+		}
+	      }
 
-            });
+	  });
 
-            try {
-
-
-                BufferedReader tradesocket = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
-                // Begin reading from socket.
-                String lineinput;
-                // String to hold each line read in.
-                String[] lineseperated;
-                // String array to seperate into individual data e.g. date, buyer, seller
-                int test100 = 0;
-                // Temporary measurement for how long to listen for.
-                while ((lineinput = tradesocket.readLine()) != null) {
-                    lineseperated = lineinput.split(",");
-                    // Splits the read line into individual data.
-                    outputTrades.println(lineseperated[0] + "," + lineseperated[1] + "," + lineseperated[2] + "," + lineseperated[3] + "," + lineseperated[4] + "," + lineseperated[5] + "," + lineseperated[6] + "," + lineseperated[7] + "," + lineseperated[8] + "," + lineseperated[9]);
-                    // Output to csvv file, note that to seperate columns need to use a "," and to seperate rows per println.
-                }
-            } finally { // Runs after try whether success or fail.
-                if (outputTrades != null) {
-                    outputTrades.close(); // Close link to file output.
-                }
-                if (echoSocket != null) {
-                    echoSocket.close(); // Close link to socket.
-                }
-            }
-        } catch (IOException IOEx) {
-            IOEx.printStackTrace();
-        }
-    }
+	  try {
+	      
+		
+		BufferedReader tradesocket = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+		// Begin reading from socket.
+		String lineinput;
+		// String to hold each line read in.
+		String[] lineseperated;
+		// String array to separate into individual data e.g. date, buyer, seller
+		if (DoesExist)
+		{
+		  lineinput = tradesocket.readLine(); // ignore first line
+		}
+		while ((lineinput = tradesocket.readLine()) != null) {
+		    lineseperated = lineinput.split(",");
+		    // Splits the read line into individual data.
+		    outputTrades.append(lineseperated[0] + "," + lineseperated[1] + "," + lineseperated[2] + "," + lineseperated[3] + "," + lineseperated[4] + "," + lineseperated[5] + "," + lineseperated[6] + "," + lineseperated[7] + "," + lineseperated[8] + "," + lineseperated[9] + "\n");
+		    // Output to csv file, note that to separate columns need to use a "," and to separate rows per println.
+		}	
+	      } finally { // Runs after try whether success or fail.
+		if (outputTrades != null) {
+		    outputTrades.close(); // Close link to file output.
+		}
+		if (echoSocket != null) {
+		    echoSocket.close(); // Close link to socket.
+		}
+	      }
+	    }
+	  catch (IOException IOEx){
+	    IOEx.printStackTrace();
+	  }
+      }
 }
