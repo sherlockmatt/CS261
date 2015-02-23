4ec02161dd57dbe3b5766eea09c35c2e6ffa35ce
sherlockmatt <matthew.w.dawson@gmail.com>
2015-02-23 14:00:36 +0000

    Moved the threads to input package Tidied up a bit Made the file reading more effective (deletes all old files)

---
+++ src/com/cs261/input/TradesThread.java
@@ -0,0 +1,75 @@
+package com.cs261.input;
+
+import java.io.*;
+import java.net.Socket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+// This code takes the input stream and then outputs it to a csv file. However now does it within a thread so other code may be used. Also it will now run until an interrupt is used (Ctrl-C).
+
+
+public class TradesThread implements Runnable {
+    public void run() { // Threads running code.
+        try {
+            System.out.println("In the trades thread");
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+            //get current date time with Date()
+            Date currentdate = new Date();
+            String dateStr = dateFormat.format(currentdate);
+            boolean DoesExist = new File(dateStr + "tradesstore.csv").exists();
+            final PrintWriter outputTrades = new PrintWriter(new FileWriter(dateStr + "tradesstore.csv", true));
+            //Define a writer for outputting trades.
+
+            String hostName = "cs261.dcs.warwick.ac.uk";
+            // Declare the host for the socket.
+            int portNumber = Integer.parseInt("80");
+            // Declare the port number to listen to.
+            final Socket echoSocket = new Socket(hostName, portNumber);
+            // Establish a socket using the chosen host and port.
+
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                // Code run when program is stopped via Ctrl+c
+                public void run() {
+                    try {
+                        System.out.println("Ctrl-c caught");
+                        outputTrades.close(); // Close link to file output.
+                        echoSocket.close(); // Close link to socket.
+                    } catch (IOException IOEx) {
+                        IOEx.printStackTrace();
+                    }
+                }
+
+            });
+
+            try {
+
+
+                BufferedReader tradesocket = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+                // Begin reading from socket.
+                String lineinput; // String to hold each line read in.
+                String[] lineseperated; // String array to separate into individual data e.g. date, buyer, seller
+                if (DoesExist) {
+                    lineinput = tradesocket.readLine(); // ignore first line if appending to file
+                }
+                while ((lineinput = tradesocket.readLine()) != null) {
+                    lineseperated = lineinput.split(","); // Splits the read line into individual data.
+                    // Output to csv file, note that to separate columns need to use a "," and to separate rows per println.
+                    for (int i = 0; i < lineseperated.length; i++) {
+                        outputTrades.append(lineseperated[i]);
+                        if (i == lineseperated.length - 1) {
+                            outputTrades.append("\n");
+                        } else {
+                            outputTrades.append(",");
+                        }
+                    }
+                }
+            } finally { // Runs after try whether success or fail.
+                outputTrades.close(); // Close link to file output.
+                echoSocket.close(); // Close link to socket.
+            }
+        } catch (IOException IOEx) {
+            IOEx.printStackTrace();
+        }
+    }
+}
--- src/com/cs261/main/Main.java
+++ src/com/cs261/main/Main.java
@@ -1,64 +1,70 @@
 package com.cs261.main;
 
-import java.io.BufferedReader;
-import java.io.PrintWriter;
-import java.io.IOException;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.text.ParseException;
-import java.io.InputStreamReader;
-import java.net.Socket;
-import java.lang.Runnable;
-import sun.misc.SignalHandler;
+import com.cs261.input.CommsThread;
+import com.cs261.input.TradesThread;
 
-import java.util.Date;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 // This code takes the input stream and then outputs it to a csv file. However now does it within a thread so other code may be used. Also it will now run until an interrupt is used (Ctrl-C).
 
 
 public class Main {
 // Implements runnable to allow the change of threads run code.
 
-    
 
     public static void main(String[] args) throws IOException {
-    	
-  	  DateFormat dateFormatForCheck = new SimpleDateFormat("yyyyMMdd");
-	   //get current date time with Date()
-	  Date currentdate = new Date();
-	  Date cutoffdate = new Date(currentdate.getTime() - 15 * 24 * 3600 * 1000);
-	  String dateStr = dateFormatForCheck.format(cutoffdate);
-	  System.out.println("Cut off date " + dateStr);
-	  
-	  File TooOldTrade = new File(dateStr + "tradesstore" + ".csv");
-	  System.out.println("Searching for " + dateStr + "tradesstore.csv");
-	  File TooOldComm = new File(dateStr + "commsstore" + ".csv");
-	  System.out.println("Searching for " + dateStr + "commsstore.csv");
-	  boolean DoesExistTrade = TooOldTrade.exists();
-	  boolean DoesExistComm = TooOldComm.exists();
-	  
-	  if (DoesExistTrade)
-		{
-		  TooOldTrade.delete();
-		  System.out.println("Deleting " + dateStr + "tradesstore.csv");
-		}
-	  if (DoesExistComm)
-		{
-		  TooOldComm.delete();
-		  System.out.println("Deleting " + dateStr + "commsstore.csv");
-		}
-	  
-	  
-      Thread Tradethread = new Thread(new TradesThread()); // Creates new thread.
-      Thread Commsthread = new Thread(new CommsThread()); // Creates new thread.
-      System.out.println("Should start the thread");
-      Tradethread.start(); // Runs new thread.
-      Commsthread.start(); // Runs new thread.
-      System.out.println("Should occur before thread finishes thread");
-      
-	    
+
+        DateFormat dateFormatForCheck = new SimpleDateFormat("yyyyMMdd");
+        //get current date time with Date()
+        Date currentdate = new Date();
+        Date cutoffdate = new Date(currentdate.getTime() - 15 * 24 * 3600 * 1000);
+        String dateStr = dateFormatForCheck.format(cutoffdate);
+        System.out.println("Cut off date " + dateStr);
+
+        File[] trades = new File("data").listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return s.matches(".+tradesstore\\.csv$");
+            }
+        });
+        File[] comms = new File("data").listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return s.matches(".+commsstore\\.csv$");
+            }
+        });
+
+        for (File file : trades) {
+            String name = file.getName();
+            Date fileDate = new Date(Integer.parseInt(name.substring(0, 4)) + 12 * Integer.parseInt(name.substring(5, 6)) + 7 * Integer.parseInt(name.substring(7, 8)));
+            if (fileDate.before(cutoffdate)) {
+                if (!file.delete()) {
+                    throw new IOException("Could not delete file " + file.getName());
+                }
+            }
+        }
+        for (File file : comms) {
+            String name = file.getName();
+            Date fileDate = new Date(Integer.parseInt(name.substring(0, 4)) + 12 * Integer.parseInt(name.substring(5, 6)) + 7 * Integer.parseInt(name.substring(7, 8)));
+            if (fileDate.before(cutoffdate)) {
+                if (!file.delete()) {
+                    throw new IOException("Could not delete file " + file.getName());
+                }
+            }
+        }
+
+        Thread Tradethread = new Thread(new TradesThread()); // Creates new thread.
+        Thread Commsthread = new Thread(new CommsThread()); // Creates new thread.
+        System.out.println("Should start the thread");
+        Tradethread.start(); // Runs new thread.
+        Commsthread.start(); // Runs new thread.
+        System.out.println("Should occur before thread finishes thread");
+
+
     }
 }
 
